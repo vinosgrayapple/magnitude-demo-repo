327d936221692f439a6949cf39b843750dfbbce6
Anders Lie <anders.erik.lie@gmail.com>
2025-03-11 22:23:42 -0400

    basic magnitude tests

---
+++ tests/magnitude.config.ts
@@ -0,0 +1,5 @@
+import { defineConfig } from "magnitude-ts";
+
+export default defineConfig({
+    baseUrl: "http://localhost:5173"
+});
---
+++ tests/todo.mag.ts
@@ -0,0 +1,20 @@
+import { test } from 'magnitude-ts';
+
+const sampleTodos = ["Take out the trash", "Buy groceries", "Build more test cases with Magnitude"];
+
+test('can add a todo')
+    .step('Enter and add a todo')
+        .data({ todo: sampleTodos[0] })
+        .check('Todo appears')
+
+test('can mark todos complete')
+    .step('Add a todo')
+    .step('Mark the todo as done')
+        .check('Check that it appears to be marked complete, and that completed count is 1')
+
+test('can add multiple todos')
+    .step('Create 3 todos')
+        .data(sampleTodos.join(", "))
+        .check('Todo total count should be 3')
+
+
